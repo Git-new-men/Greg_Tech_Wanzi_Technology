dff1f2515bb39bb5211ae3e021fe1495904fdf64
PumpkinXD <54535387+PumpkinXD@users.noreply.github.com>
2026-08-28 20:27:40 +0800

    feat(tacz):为剩下的枪包弹药恢复并修改手搓版配方
    我想手搓的弹药应该写完了

--- kubejs/server_scripts/TACZ_ammo_recipes.js
+++ kubejs/server_scripts/TACZ_ammo_recipes.js
@@ -4,7 +4,7 @@ ServerEvents.recipes((event) => {
     event.forEachRecipe({}, (r) => {
         const id = String(r.getId());
         if (id.startsWith("tacz:ammo/")) {
-            gunIds.push(id);
+            ammoIds.push(id);
         }
     });
 
@@ -383,13 +383,13 @@ ServerEvents.recipes((event) => {
                 item: {
                     tag: "forge:bolts/lead",
                 },
-                count: 5,
+                count: 9,
             },
             {
                 item: {
                     tag: "forge:foils/copper",
                 },
-                count: 4,
+                count: 5,
             },
             {
                 item: {
@@ -858,5 +858,533 @@ ServerEvents.recipes((event) => {
     });
 });
 
-//TODO: Helldiver2 AMMO recipes
-//TODO: Create Armorer && Immersive Armorer AMMO Recipes
+//ammo recipes for third-party packs
+ServerEvents.recipes((event) => {
+    const ammoIds = [];
+    event.forEachRecipe({}, (r) => {
+        const id = String(r.getId());
+        if (
+            id.startsWith("helldiver2:ammo/") ||
+            id.startsWith("create_armorer:ammo/") ||
+            id.startsWith("immersive_armorer:ammo/")
+        ) {
+            ammoIds.push(id);
+        }
+    });
+
+    ammoIds.forEach((id) => {
+        event.remove({ id: id });
+        console.info(`Removed ammo recipe: ${id}`);
+    });
+
+    //Helldiver2 AMMO recipes
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/vanadium_steel",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/lead",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 15,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "helldiver2:pistol_normal",
+            count: 30,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/vanadium_steel",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 6,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 8,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "helldiver2:rifle_normal",
+            count: 15,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:nuggets/lead",
+                },
+                count: 20,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 5,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 8,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "helldiver2:shotgun_normal",
+            count: 10,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/tungsten_steel",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 6,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 8,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "helldiver2:rifle_ap",
+            count: 15,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/tungsten_steel",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 20,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 30,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "helldiver2:explode_ap",
+            count: 15,
+        },
+    });
+
+    //Create Armorer && Immersive Armorer BULLET recipes
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/lead",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 4,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:454_casul",
+            count: 15,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/lead",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 8,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "create_armorer:gas_pistol_ammo",
+            count: 30,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/lead",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 8,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 5,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "create_armorer:rbapb",
+            count: 16,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/steel",
+                },
+                count: 30,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 20,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 10,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "create_armorer:slap",
+            count: 30,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:bolts/vanadium_steel",
+                },
+                count: 30,
+            },
+            {
+                item: {
+                    tag: "forge:foils/lead",
+                },
+                count: 10,
+            },
+            {
+                item: {
+                    tag: "forge:foils/copper",
+                },
+                count: 10,
+            },
+            {
+                item: {
+                    tag: "forge:plates/brass",
+                },
+                count: 15,
+            },
+            {
+                item: {
+                    item: "gtceu:dynamite",
+                },
+                count: 10,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:ap_ammo",
+            count: 30,
+        },
+    });
+
+    //Immersive Armorer special AMMO recipes
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:double_plates/stainless_steel",
+                },
+                count: 16,
+            },
+            {
+                item: {
+                    item: "gtceu:gelled_toluene",
+                },
+                count: 16,
+            },
+            {
+                item: {
+                    tag: "forge:dusts/blaze",
+                },
+                count: 32,
+            },
+            {
+                item: {
+                    type: "forge:partial_nbt",
+                    item: "gtceu:steel_fluid_cell",
+                    nbt: {
+                        Fluid: {
+                            Amount: 8000,
+                            FluidName: "gtceu:gasoline",
+                        },
+                    },
+                },
+                count: 2,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:chemical",
+            count: 16,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    tag: "forge:rods/steel",
+                },
+                count: 8,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:cut_steel_rod",
+            count: 16,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    type: "forge:partial_nbt",
+                    item: "gtceu:tantalum_capacitor",
+                    nbt: {
+                        Charge: 1000,
+                    },
+                },
+                count: 16,
+            },
+            {
+                item: {
+                    item: "enderio:basic_capacitor",
+                },
+                count: 16,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:burst_capacitor",
+            count: 16,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    item: "gtceu:lv_emitter",
+                },
+                count: 1,
+            },
+            {
+                item: {
+                    type: "forge:partial_nbt",
+                    item: "gtceu:tantalum_capacitor",
+                    nbt: {
+                        Charge: 1000,
+                    },
+                },
+                count: 32,
+            },
+            {
+                item: {
+                    item: "enderio:double_layer_capacitor",
+                },
+                count: 16,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:stream_capacitor",
+            count: 16,
+        },
+    });
+
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            {
+                item: {
+                    item: "gtceu:mv_emitter",
+                },
+                count: 1,
+            },
+            {
+                item: {
+                    type: "forge:partial_nbt",
+                    item: "gtceu:tantalum_capacitor",
+                    nbt: {
+                        Charge: 1000,
+                    },
+                },
+                count: 64,
+            },
+            {
+                item: {
+                    item: "enderio:octadic_capacitor",
+                },
+                count: 16,
+            },
+        ],
+        result: {
+            type: "ammo",
+            id: "immersive_armorer:high_power_capacitor",
+            count: 8,
+        },
+    });
+});
